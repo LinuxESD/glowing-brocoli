155671d96b34581e08216ca941912b2d16473dd5
LinuxESD <161594882+LinuxESD@users.noreply.github.com>
2024-09-04 19:09:35 +0530

    makefile changes for adding service

---
+++ eightman.mk
@@ -0,0 +1,16 @@
+    DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/generic/goldfish/device_framework_matrix.xml
+    
+    
+    # aidl_interface
+    PRODUCT_PACKAGES += vendor.eightman.cpu
+
+    # cc_binary
+    PRODUCT_PACKAGES += vendor.eightman.cpu-service
+
+    # cc_binary
+    PRODUCT_PACKAGES_DEBUG += eightman_cpu_tester
+
+    # android_app
+    #PRODUCT_PACKAGES += EightmanServices
+
+    
